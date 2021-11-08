1bd015bc9d867377be29250133d9a2d9d9c82f5f
Danny Caulwell <caulwell@protonmail.com>
2021-11-08 15:51:20 +0000

    further fix control menu onClickAway

--- src/components/Control/Control.js
+++ src/components/Control/Control.js
@@ -78,49 +78,49 @@ export default function Control({type, props}){
             case "Tree Type":
                 return ( 
                     <>
-                        <Button size="small" color="inherit" variant="outlined" component={Link} to={"/bst"}>BST</Button>
-                        <Button size="small" color="inherit" variant="outlined" component={Link} to={"/avl"}>AVL</Button>
-                        <Button size="small" color="inherit" variant="outlined" component={Link} to={"/red-black"}>Red-Black</Button>
+                        <Button size="small" color="inherit" variant="outlined" data-close={true} component={Link} to={"/bst"}>BST</Button>
+                        <Button size="small" color="inherit" variant="outlined" data-close={true} component={Link} to={"/avl"}>AVL</Button>
+                        <Button size="small" color="inherit" variant="outlined" data-close={true} component={Link} to={"/red-black"}>Red-Black</Button>
                     </>
                 )
             case "New Node":
                 return (
                     <>
-                    <TextField className="popper-menu-item-input" size="small" type="number" label="New node" autoComplete="off" value={props.addInput} variant="filled" name="addInput" onChange={e => props.setAddInput(e.target.value)} inputProps={{maxLength: 4, }}  onKeyPress={props.handleKeypress}/>
-                    <Button size="small" className="popper-menu-item-button" variant="contained" name="addButton" onClick={props.handleButtonPress}>Add</Button>
+                    <TextField className="popper-menu-item-input" size="small"  type="number" label="New node" autoComplete="off" value={props.addInput} variant="filled" name="addInput" onChange={e => props.setAddInput(e.target.value)} inputProps={{maxLength: 4, "data-close": false }}  onKeyPress={props.handleKeypress}/>
+                    <Button size="small" className="popper-menu-item-button" data-close={false} variant="contained" name="addButton" onClick={props.handleButtonPress}>Add</Button>
                     </>
                 )
             case "Search":
                 return (
                     <>
-                    <TextField  className="popper-menu-item-input" size="small" type="number" label="Search" autoComplete="off"  value={props.searchInput} variant="filled" name="searchInput" onChange={e => props.setSearchInput(e.target.value)} inputProps={{maxLength: 4, }} onKeyPress={props.handleKeypress}/>
-                    <Button size="small" className="popper-menu-item-button" variant="contained" name="searchButton" onClick={props.handleButtonPress}>Search</Button>
+                    <TextField  className="popper-menu-item-input" size="small" type="number" label="Search" autoComplete="off"  value={props.searchInput} variant="filled" name="searchInput" onChange={e => props.setSearchInput(e.target.value)} inputProps={{maxLength: 4, "data-close": false }} onKeyPress={props.handleKeypress}/>
+                    <Button size="small" className="popper-menu-item-button" data-close={false} variant="contained" name="searchButton" onClick={props.handleButtonPress}>Search</Button>
                     </>
                 )
 
             case "Animation Speed":
                 return (
                     <>
-                    <Slider onChange={props.handleSpeedChange} onChangeCommitted={props.handleAnimationSpeed} min={0.1} max={1} step={0.1} value={props.speedInput} valueLabelDisplay="auto" marks/>
+                    <Slider onChange={props.handleSpeedChange} data-close={false} onChangeCommitted={props.handleAnimationSpeed} min={0.1} max={1} step={0.1} value={props.speedInput} valueLabelDisplay="auto" marks/>
                     </>
                 )
             case "Save Tree":
                 return (
                     <>
-                    <TextField className="popper-menu-item-input" size="small" type="text" label="TreeName" autoComplete="off" variant="filled" name="treeNameInput"/>
-                    <Button sx={{height: "30px"}} onClick={props.handleButtonPress} variant="contained" name="saveButton" size="small">Save</Button>
+                    <TextField className="popper-menu-item-input" data-close={false} size="small" type="text" label="TreeName" autoComplete="off" variant="filled" name="treeNameInput" inputProps={{"data-close":false}}/>
+                    <Button sx={{height: "30px"}} data-close={false} onClick={props.handleButtonPress} variant="contained" name="saveButton" size="small">Save</Button>
                     </>
                 )
             case "Upload CSV":
                 return(
                     <>
-                    <label htmlFor="contained-button-file">
-                        <Input sx={{height: "30px"}} style={{display: "none"}} accept="*.csv" id="contained-button-file" type="file" onChange={props.handleFileInput} size="small"/>
-                        <Button sx={{height: "30px"}} variant="contained" component="span" size="small">
+                    <label htmlFor="contained-button-file" data-close={false}>
+                        <Input sx={{height: "30px"}} inputProps={{"data-close": false}} style={{display: "none"}} accept="*.csv" id="contained-button-file" type="file" onChange={props.handleFileInput} size="small"/>
+                        <Button sx={{height: "30px"}} data-close={false} variant="contained" component="span" size="small">
                         Upload
                         </Button>
                     </label>
-                    <Button sx={{height: "30px"}} variant="contained" size="small"  onClick={props.handleButtonPress} name="generateButton">
+                    <Button sx={{height: "30px"}} data-close={false} variant="contained" size="small"  onClick={props.handleButtonPress} name="generateButton">
                         Generate
                     </Button>
                     </>
@@ -133,8 +133,13 @@ export default function Control({type, props}){
     const open = Boolean(anchorEl);
     const id = open ? type : undefined;
 
-    const handleClickAway = () => {
-        setAnchorEl(null);
+    const handleClickAway = (e) => {
+        if(e.target.getAttribute("data-close") === null || e.target.getAttribute("data-close") === true){
+            setAnchorEl(null);
+        } else {
+            return;
+        }
+        
     }
 
 
@@ -157,8 +162,8 @@ export default function Control({type, props}){
 
         {popperTrue && 
         
-        <Popper id={id} open={open} anchorEl={anchorEl} placement="right">
-                    <Paper sx={{width: "350px", height: "65px", display: "flex", justifyContent: "space-evenly", alignItems: "center", paddingLeft: "10px", paddingRight: "10px"}}>
+        <Popper id={id} open={open} anchorEl={anchorEl} placement="right" data-close={false}>
+                    <Paper data-close={false} sx={{width: "350px", height: "65px", display: "flex", justifyContent: "space-evenly", alignItems: "center", paddingLeft: "10px", paddingRight: "10px"}}>
                         {getPopperContent()}
                     </Paper>
             </Popper>
