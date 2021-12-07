e25b320a3920f438965007d066f2caef1b4eb38c
Danny Caulwell <caulwell@protonmail.com>
2021-12-07 19:11:10 +0000

    add welcome page responsiveness

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,10 +6,7 @@ export default function Header(){
     return (
 
       <div className="header">
-          <div className="title">
             <h3><Link to="/">TreeVis</Link></h3>
-            <h4><Link to="/bst">Tree</Link></h4>
-          </div>
         </div>
 
 
